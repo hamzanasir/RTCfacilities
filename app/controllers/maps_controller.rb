fe5fbb7f378efb88ae0b390cb8b4e452fc09f4f6
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-02 04:40:08 +0000

    Duplicated stuart get and post logic for Alumini. Should condense this logic in one action if need for seperate logic doesn't arise

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,5 @@
 class MapsController < ApplicationController
   def stuart
-    puts params
     if params[:flash] == nil
       @flash = {}
     else
@@ -31,5 +30,32 @@ class MapsController < ApplicationController
   end
 
   def alumini
+    if params[:flash] == nil
+      @flash = {}
+    else
+      @flash = params[:flash]
+    end
+  end
+  
+  def alumini_room
+    room = Room.where(roomNumber: "AM-#{params[:roomNumber]}").take
+    if room.nil?
+      render json: {'error' => 'true'}
+    else
+      render json: room.to_json(:include => :complaints )
+    end
+  end
+  
+  def alumini_room_post
+    complaint = Complaint.new;
+    complaint.complaint = params[:complaint]
+    complaint.higher = params[:higher] == 'higher'
+    complaint.satisfied = false
+    complaint.room = Room.where(roomNumber: "AM-#{params[:roomNumber]}").take
+    if complaint.save
+      redirect_to action: 'alumini', :flash => { :success => "Your request was successfully submitted." }
+    else
+      redirect_to action: 'alumini', :flash => { :error => "Request Failed." }
+    end
   end
 end
