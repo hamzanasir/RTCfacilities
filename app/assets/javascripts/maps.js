9361ae86106e829ee728f5c467b3a71490b65b12
Hamza Nasir <mnasir@hawk.iit.edu>
2018-04-10 06:03:50 +0000

    Functions to render real life beacons using data in meters

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -184,3 +184,57 @@ function renderSVG (mobile, svgName, initialRender) {
     }
   });
 }
+
+// Functions for mapping real life becons on Map
+function setLocation(x, y, radius) {
+    d3.select('svg').append("circle")
+                    .attr("cx", x)
+                    .attr("cy", y)
+                    .attr("r", 15);
+
+    d3.select('svg').append("circle")
+                    .attr("cx", x)
+                    .attr("cy", y)
+                    .style("fill", "#5e8fd1")
+                    .style("fill-opacity", "0.59")
+                    .attr("r", 10)
+                    .transition()
+                    .duration(750)
+                    .attr("r", radius);
+}
+
+// Gets aspect ratio for SVG Map. Should be same as real aspect ration of building floor.
+function getAspectRatio() {
+  const origin = d3.select('.origin').filter('path').node().getBBox();
+  const originTop = d3.select('.originTop').filter('path').node().getBBox();
+  return ((origin.y + origin.height) - originTop.y)/((originTop.x + originTop.width)- origin.x)
+}
+
+// Gets aspect ratio for real building floor from meta-data embedded in SVG maps
+function getRealAspectRatio() {
+  return parseFloat(d3.select('svg').attr('data-height'), 10)/parseFloat(d3.select('svg').attr('data-width'), 10)
+}
+
+function mapX (x) {
+  const origin = d3.select('.origin').filter('path').node().getBBox();
+  const originTop = d3.select('.originTop').filter('path').node().getBBox();
+  const in_min = 0;
+  const in_max = parseFloat(d3.select('svg').attr('data-width'), 10);
+  const out_min = origin.x;
+  const out_max = originTop.x + originTop.width;
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+function mapY (y) {
+  const origin = d3.select('.origin').filter('path').node().getBBox();
+  const originTop = d3.select('.originTop').filter('path').node().getBBox();
+  const in_min = 0;
+  const in_max = parseFloat(d3.select('svg').attr('data-height'), 10);
+  const out_min = origin.y + origin.height;
+  const out_max = originTop.y;
+  return (y - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+function setBeacon(x,y) {
+  setLocation(mapX(x), mapY(y), 100);
+}
